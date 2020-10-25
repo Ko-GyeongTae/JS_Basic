37c7748ee7bb1a1d60eec6281a3d87463c55562f
Ko-Gyeongtae <kokt0203@naver.com>
2020-10-25 22:40:40 +0900

    add - http server 만들기

--- webserver.js
+++ webserver.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const { runInNewContext } = require('vm');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    //console.log(req.url);
+    if(req.url == "/"){
+        res.write("<h1>Hello from node js</h1>");
+        res.end();
+    }
+    else{
+        res.write(`<h1>You have entered this url : ${req.url}</h1>`);
+    }
 });
 
-server.listen(3000, () =>{
-    console.log("The Server is listening on port 3000");
-})
+server.listen(5500, () => {
+    console.log("The Server is listening on port 5500");
+});
