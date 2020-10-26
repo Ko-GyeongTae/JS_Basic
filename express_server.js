c863b636ff01c3577b1cd12b5f07fb93c2426e1d
Ko-Gyeongtae <kokt0203@naver.com>
2020-10-26 11:41:08 +0900

    edit - express_server.js complete npm i err

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { createServer } = require("http");
 const server = express();
 
-server.get("");
+server.get("/", (req, res) => {
+    res.send("<h1>Hello from nodejs</h1>")
+});
 
-server.listen(3000, () => {
+server.listen(3000, (err) => {
     if(err) return console.log(err);
     console.log("The server is listening on 3000 port!");
-})
+});
