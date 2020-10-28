5a32f8ddf2454ca357fa9bbe22718460c503f9c8
Ko-Gyeongtae <kokt0203@naver.com>
2020-10-28 22:20:57 +0900

    add - template environmental

--- express_server.js
+++ express_server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const { createServer } = require("http");
+const expressHandlebars = require('express-handlebars');
+
 const server = express();
 
+server.engine("hbs", expressHandlebars({
+        extname:"hbs",
+        defaultLayout:"layout.hbs",
+        partialsDir:"partials", 
+    })
+);
+server.set("view engine", "hbs");
 server.use(express.static(__dirname + "/public"));
 
+server.get("/", (req, res)=>{
+    res.render("home");
+});
+/*
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
@@ -15,7 +28,7 @@ server.get("/about", (req, res) => {
 server.use((req, res)=>{
     res.sendFile(__dirname + "/404.html");
 });
-
+*/
 server.listen(3000, (err) => {
     if(err) return console.log(err);
     console.log("The server is listening on 3000 port!");
