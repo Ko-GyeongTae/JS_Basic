bd65cae95ffbe3fdb0295f4a6fde81fa5fd4d581
Ko-Gyeongtae <kokt0203@naver.com>
2020-11-08 02:28:03 +0900

    delete - unuseful code

--- express_server.js
+++ express_server.js
@@ -43,18 +43,3 @@ server.listen(3000, (err) => {
     if(err) return console.log(err);
     console.log("The server is listening on 3000 port!");
 });
-
-
-/*
-server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
-server.get("/about", (req, res) => {
-    res.sendFile(__dirname + "/about.html");
-});
-
-server.use((req, res)=>{
-    res.sendFile(__dirname + "/404.html");
-});
-*/
